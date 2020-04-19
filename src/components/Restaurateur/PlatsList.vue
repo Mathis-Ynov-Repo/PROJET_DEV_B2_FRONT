155bfe8dbb0bff567a145cebc885e68d03492a91
EyZex <eyzex@hotmail.fr>
2020-04-19 16:17:18 +0200

    ajout fonction ajout plat restaurateur

--- src/components/Restaurateur/PlatsList.vue
+++ src/components/Restaurateur/PlatsList.vue
@@ -15,6 +15,44 @@
         <Plat v-for="plat in OwnerRestaurant.plats" :key="plat.id" :plat="plat" />
       </v-row>
     </v-container>
+    <v-card-text class="white" style="height: 100px; position: relative">
+      <v-btn color="primary" dark absolute top right fab @click="dialog = !dialog">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-card-text>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-form v-model="valid">
+        <v-card>
+          <v-card-title>
+            <span class="headline">Ajouter votre plat</span>
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="platTitle"
+              :rules="[v => !!v || 'Un libelle est requis']"
+              label="Libelle du plat"
+              required
+            ></v-text-field>
+            <v-text-field prefix="$" v-model="platPrice" :rules="priceRules" label="Prix" required></v-text-field>
+
+            <v-select
+              v-model="platSelect"
+              :items="types"
+              item-text="libelle"
+              item-value="@id"
+              :rules="[v => !!v || 'Ce champ est requis']"
+              label="Type de votre plat"
+              required
+            ></v-select>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn text color="primary" :disabled="!valid" @click="save">Sauvegarder</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </v-dialog>
   </section>
 </template>
 
@@ -24,7 +62,18 @@ import Plat from "./Plat";
 export default {
   data() {
     return {
-      loading: true
+      loading: true,
+      valid: true,
+      types: [],
+      platSelect: "",
+      platTitle: "",
+      platPrice: "",
+
+      priceRules: [
+        v => !!v || "Un prix est requise",
+        v => !isNaN(v) || "Veuillez entrer une valeur numérique"
+      ],
+      dialog: false
     };
   },
   components: {
@@ -35,6 +84,10 @@ export default {
   },
   mounted() {
     this.getRestaurant(this.$store.getters["authentication/authUser"].id);
+
+    this.$http
+      .get("http://localhost:3000/api/plats_types")
+      .then(response => (this.types = response.data["hydra:member"]));
   },
   methods: {
     // initialize() {
@@ -44,6 +97,22 @@ export default {
     //   );
     //   this.loading = false;
     // },
+    async save() {
+      this.loading = true;
+      await this.$http
+        .post("http://localhost:3000/api/plats", {
+          libelle: this.platTitle,
+          prix: parseFloat(this.platPrice),
+          platType: this.platSelect
+        })
+        .then()
+        .catch(e => {
+          this.errors.push(e);
+        });
+      await this.getRestaurant();
+      this.loading = false;
+      this.dialog = false;
+    },
     ...mapActions({
       getRestaurant: "restaurant/getRestaurantWithOwner"
     })
